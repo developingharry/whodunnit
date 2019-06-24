90c1d5e5913692713dc5611d855a43a7add3990c
Harry Hunt <developingharry@outlook.com>
2019-06-24 21:11:58 +0100

    php and sql connected, query needs fix

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+include("includes/includedFiles.php"); 
+?>
+
+
+<div class="gridViewContainer">
+	<?php
+		$detectiveQuery = mysqli_query($con, "SELECT * FROM detectives ORDER BY RAND() LIMIT 10");
+		while($row = mysqli_fetch_array($detectiveQuery)) {
+            echo "hello";
+        }
+	?>
+</div>
+

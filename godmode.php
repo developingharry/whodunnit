4d96ee34fa0cededc367b5b3779e079f98b099c4
Harry Hunt <developingharry@outlook.com>
2019-06-26 20:38:38 +0100

    now shows suspects with a bit more clarity

--- godmode.php
+++ godmode.php
@@ -1,17 +1,22 @@
 <?php
 include("includes/header.php");
 
-$sql = "SELECT id, forename, surname, alive, guilt FROM suspects";
+echo "<h1>GOD POWER PAGE</h1>";
+$sql = "SELECT id, forename, surname, alive, guilt, alivepic, deadpic FROM suspects";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
-    echo "Suspects:<br>";
+    echo "<h2>Suspects:</h2>";
 
     while($row = $result->fetch_assoc()) {
         // convert booleans to readable strings
         $aliveString = ($row["alive"]) ? 'alive' : 'dead';
         $guiltString = ($row["guilt"]) ? 'guilty' : 'innocent';
         // display suspect
+        // which image to show?
+        $imgUrl = ($row['alive']) ? $row['alivepic'] : $row['deadpic'];
+        echo "<img src='".$imgUrl."'>";
+
         echo $row["id"]. ". " . $row["forename"]. " " .$row["surname"]. " : ".$aliveString." + ". $guiltString;
         // kill/rez button
         echo "<br><button class='button' onclick='lifeToggle(";
@@ -20,7 +25,7 @@ if ($result->num_rows > 0) {
         // acquit/implicate button
         echo "<button class='button' onclick='guiltToggle(";
         $guiltLabel = ($row['guilt']) ? 'ACQUIT' : 'IMPLICATE';
-        echo $row['id'].")'>".$guiltLabel."</button><br>";
+        echo $row['id'].")'>".$guiltLabel."</button><hr>";
         }
 } else {
     echo "0 results";

dff2ae35c8a5ee84feba1050b8d5f0108a6a0473
Harry Hunt <developingharry@outlook.com>
2019-06-28 13:44:06 +0100

    login/reg set up

--- register.php
+++ register.php
@@ -1,3 +1,34 @@
+<?php
+
+    function sanitizeFormUsername($inputText) {
+        $inputText = strip_tags($inputText);
+        $inputText = str_replace(" ", "", $inputText);
+        return $inputText;
+    }
+
+    function sanitizeFormEmail($inputText) {
+        $inputText = strip_tags($inputText);
+        $inputText = str_replace(" ", "", $inputText);
+        return $inputText;
+    }
+
+    function sanitizeFormPassword($inputText) {
+        $inputText = strip_tags($inputText);
+        return $inputText;
+    }
+
+    if(isset($_POST['loginButton'])) {
+        //login button was pressed
+    }
+    if(isset($_POST['registerButton'])) {
+        //register button was pressed
+        $username = sanitizeFormUsername($_POST['username']);
+        $email = sanitizeFormEmail($_POST['email']);
+        $email2 = sanitizeFormEmail($_POST['email2']);
+        $password = sanitizeFormPassword($_POST['password']);
+        $password2 = sanitizeFormPassword($_POST['password']);
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
